a492d06ccbb58c5b1e8f0e9cdb06e6d56e218ed5
Gal Elmalah <gale@wix.com>
2019-04-26 22:11:23 +0300

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import { Fetch } from './ReactHttp/index';
+import { Cache } from './ReactHttp/Cache';
 export default Fetch;
-export { Fetch };
+export { Fetch, Cache };
--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = require("./ReactHttp/index");
 exports.Fetch = index_1.Fetch;
+var Cache_1 = require("./ReactHttp/Cache");
+exports.Cache = Cache_1.Cache;
 exports.default = index_1.Fetch;
